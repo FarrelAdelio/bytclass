217cfa9bd5565fb41beeb8256b90ac9831291d14
FarrelAdelio <farreladelioasm@gmail.com>
2024-12-15 15:41:01 +0700

    main

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins": ["Poppins"]
+      },
+      fontsize: {
+        '10xl': '120px'
+      },
+      width: {
+        '500': '500px'
+      },
+      width: {
+        '550': '550px'
+      },
+      width: {
+        '40': '40%'
+      },
+      width: {
+        '35': '35%'
+      },
+      height: {
+        '500': '500px'
+      },
+      height: {
+        '400': '400px'
+      },
+      height: {
+        '250': '250px'
+      },
+    },
+  },
+  plugins: [],
+}
